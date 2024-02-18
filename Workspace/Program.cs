17fa487bf45752255ad7eb4c67ec7f6d10714ed8
Birkan Karaer <karaermustafabirkan@gmail.com>
2024-02-18 23:52:42 +0300

    Add project files.

---
+++ Workspace/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Workspace
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{ 
+			
+        }
+
+		static void SelamVer(string isim = "isimsiz")
+		{
+			Console.WriteLine("Merhaba " + isim);
+		}
+		static int Topla()
+		{
+			return 5;
+		}
+	}
+}
